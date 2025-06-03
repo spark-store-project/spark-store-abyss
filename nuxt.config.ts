215afd8f24f394b9a4fa96acb21d1a7b73ec7802
jiwangyihao <jwyh@jwyihao.top>
2025-06-04 01:43:25 +0800

    feat: 将暗黑模式初始化逻辑嵌入配置文件，移除独立脚本以加快脚本执行，减少画面闪烁

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,7 +40,18 @@ export default defineNuxtConfig({
       ],
       script: [
         {
-          src: "/init_dark.js",
+          innerHTML: `const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const darkMode = localStorage.getItem("darkMode");
+const sDarkValue =
+  darkMode === "auto"
+    ? isDarkMode
+      ? true
+      : false
+    : darkMode === "dark"
+    ? true
+    : false;
+
+document.documentElement.classList.toggle("s-dark", sDarkValue);`,
         },
       ],
     },
